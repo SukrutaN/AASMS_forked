0436eb6bebb5822a81155408d7ebb2372c213bd3
Alan-J-Bibins <alanjbibins@gmail.com>
2026-03-08 21:54:43 +0530

    feat: Initialized project with pio and setup wokwi

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <Arduino.h>
+
+void setup() {
+    // So the code here runs only once, this section is to be used to setup pins and configure the microcontroller
+}
+
+void loop() {
+    // This section contains the code that runs on loop (hence the name of the function)
+}
